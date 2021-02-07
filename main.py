310cfdf77ec03a333cbf6fa9f12bb540ad3345be
seondongpyo <psd1390@gmail.com>
2021-02-07 15:10:12 +0900

    공동인증서 로그인으로 방식 변경

--- main.py
+++ main.py
@@ -8,27 +8,30 @@ from util.selenium_process_manager import kill_selenium_chrome_driver
 
 try:
     driver = webdriver.Chrome(ChromeDriverManager().install())  # 셀레늄 드라이버 초기화
-    wait = WebDriverWait(driver, 10)  # 명시적 대기를 위한 WebDriverWait 생성
+    wait = WebDriverWait(driver, 5)  # 명시적 대기를 위한 WebDriverWait 생성
 
     # 홈택스 메인 페이지로 이동
     hometax_url = 'https://hometax.go.kr/websquare/websquare.html?w2xPath=/ui/pp/index.xml'
     driver.get(hometax_url)
 
-    # 홈택스 로그인 페이지로 이동
-    wait.until(ec.presence_of_element_located((By.ID, 'group1295')))  # 페이지 로딩을 위한 대기
-    wait.until(ec.presence_of_element_located((By.ID, 'wfFooter')))
-    login_button = wait.until(ec.element_to_be_clickable((By.ID, 'group88615548')))  # 로그인 버튼이 보일 때까지 대기
-    login_button.click()
+    # 연말정산간소화 페이지로 이동
+    navigate_to_simplified_page = wait.until(ec.presence_of_element_located((By.ID, 'menuAtag_0112100000')))
+    driver.execute_script('arguments[0].click()', navigate_to_simplified_page)
+    wait.until(ec.title_is('국세청 홈택스 - 연말정산간소화'))
 
-    # 아이디, 비밀번호 입력하여 로그인 시도
-    wait.until(ec.title_is('국세청 홈택스 - 로그인'))
+    # 공동인증서 로그인 클릭
     wait.until(ec.frame_to_be_available_and_switch_to_it((By.ID, 'txppIframe')))
-    driver.find_element_by_id('iptUserId').send_keys('아이디')
-    driver.find_element_by_id('iptUserPw').send_keys('비밀번호')
-    driver.find_element_by_id('anchor6').click()
+    login_button = wait.until(ec.element_to_be_clickable((By.ID, 'textbox103999')))
+    login_button.click()
+    
+    # 로딩 대기
+    wait.until(ec.presence_of_element_located((By.ID, '___processbar2_i')))
+    wait.until(ec.invisibility_of_element((By.ID, '___processbar2_i')))
+    login_by_certificate_button = wait.until(ec.element_to_be_clickable((By.ID, 'group91882124')))
+    login_by_certificate_button.click()
 
     # chromedriver.exe 종료
     kill_selenium_chrome_driver()
 
 except Exception as e:
-    print(e)
+    print('예외 발생', e)
--- util/selenium_process_manager.py
+++ util/selenium_process_manager.py
@@ -3,19 +3,15 @@ import psutil
 
 def kill_selenium_chrome_driver():
     for process in psutil.process_iter():
-        try:
-            process_name = process.name()
-            process_id = process.pid
+        process_name = process.name()
+        process_id = process.pid
 
-            if process_name == "chromedriver.exe":
-                psutil.Process(pid=process_id).kill()
-                
-                # 부모 프로세스까지 종료하려면 주석 해제
-                # parent_pid = process_id
-                # parent = psutil.Process(parent_pid)
-                # for child in parent.children(recursive=True):
-                #     child.kill()
-                # parent.kill()
+        if process_name == "chromedriver.exe":
+            psutil.Process(pid=process_id).kill()
 
-        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-            pass
+            # 부모 프로세스까지 종료하려면 주석 해제
+            # parent_pid = process_id
+            # parent = psutil.Process(parent_pid)
+            # for child in parent.children(recursive=True):
+            #     child.kill()
+            # parent.kill()

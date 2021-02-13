199b79a3bd9c4b3d69d23e2dfbfdc30023133062
seondongpyo <psd1390@gmail.com>
2021-02-13 19:18:42 +0900

    Page Object Model 적용 시작

---
+++ pages/HometaxMainPage.py
@@ -0,0 +1,16 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ec
+
+
+class HometaxMainPage:
+    BUTTON_NAVIGATE_TO_SIMPLIFIED_PAGE = (By.ID, 'menuAtag_0112100000')
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
+
+    def navigate_to_simplified_page(self):
+        button = self.wait.until(ec.presence_of_element_located(self.BUTTON_NAVIGATE_TO_SIMPLIFIED_PAGE))
+        self.driver.execute_script('arguments[0].click()', button)
+        self.wait.until(ec.title_is('국세청 홈택스 - 연말정산간소화'))
